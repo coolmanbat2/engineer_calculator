5e8de9f7689604aeb6f472f69d45df382697b22c
coolmanbat2 <thanusun.thayaparan@mail.utoronto.ca>
2019-10-19 05:55:18 -0400

    Made changes to test.py for clarity.

--- test.py
+++ test.py
@@ -1,35 +1,38 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 
-app = Flask(__name__)
+class FormulaInterface():
+    """
+    Formula interface that provides conveinence in creating equations
+    for other developers.
+    Author: Thanusun
+    Date: October 18th, 2019
+    """
+    def formula(**args):
+        raise NotImplementedError("You Must Implement This :)")
 
-@app.route("/")
-# app = Flask(__name__,template_folder='E:\Programming\Python Projects\Flask')
-def index():
-    return "Flask App!"
-	
 # Area of a Square formula.
-def areaOfSquareFormula(length, width): 
-	return int(length)*int(width)
-	
-	
-# Finds the area, then posts it on the page. 
+class Square(FormulaInterface):
+    """
+    Provides all the properties of a Square, including its formula.
+    """
+    def formula(length, width):
+        return int(length)*int(width)
+
+# Finds the area, then posts it on the page.
 @app.route('/areasquare/', methods=['GET', 'POST'])
-def areas(): 
-	
+def areas():
+
 	areaValue = ""
 	length = ""
 	width = ""
-	if request.method == 'POST': 
+	if request.method == 'POST':
 		length = request.form['length']
 		width = request.form['width']
-		areaValue = areaOfSquareFormula(length, width)
-		
-	return render_template('mathcalc.html', areaValue=areaValue, 
-											length=length, 
+		areaValue = Square.formula(length, width)
+
+	return render_template('mathcalc.html', areaValue=areaValue,
+											length=length,
 											width=width)
 
-	
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
-	
-	
